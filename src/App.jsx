18dfc7a4169fe5c83d1a8fcb3cabcae3880f9caf
Starkk <42775517+prabal-007@users.noreply.github.com>
2024-10-23 03:09:36 +0530

    Added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Main from "./components/Main"
 import RevenueCard from "./components/RevenueCard"
+import Sidebar from "./components/Sidebar"
 import Topbar from "./components/Topbar"
 
 function App() {
-  
+
   return (
     <>
-    <Main />
+      <div>
+        <Sidebar />
+        <div className="sm:ml-64">
+                <Main />
+            </div>
+      </div>
     </>
   )
 }
